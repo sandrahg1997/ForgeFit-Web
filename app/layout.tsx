cf167cd4f913dda29d31fa2fa46a5c8ff63bce2f
sandrahg1997 <sandrahg1997@gmail.com>
2026-08-04 13:25:52 +0200

    cambio de icono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
-export const metadata: Metadata = { title:"ForgeFit", description:"Entrenamiento, progreso y récords", manifest:"/manifest.webmanifest", icons:{icon:"/icon.svg"} };
+export const metadata: Metadata = { title:"ForgeFit", description:"Entrenamiento, progreso y récords", manifest:"/manifest.webmanifest", icons:{icon:"/icon.jpeg"} };
 export const viewport: Viewport = { themeColor:"#0b1120", colorScheme:"dark" };
 export default function RootLayout({children}:{children:React.ReactNode}) { return <html lang="es"><body>{children}</body></html>; }
